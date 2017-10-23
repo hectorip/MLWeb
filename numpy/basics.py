dd0655aefd507eab95c6194e20a5bac36ef0db4b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-10-22 23:56:25 -0500

    Explicaciones y suma

--- numpy/basics.py
+++ numpy/basics.py
@@ -4,6 +4,8 @@ import numpy as np
 data = np.array([1, 1, 3, 5, 8, 13])
 
 print(data)
+
+# Statistical data is availabe in the arrays
 print(data.mean())
 print(data.sum())
 
@@ -13,11 +15,15 @@ matrix = np.array([
 ])
 
 print(matrix)
+
+# Transpose
 print(matrix.T)
 
+# dot Product
 print(matrix.dot(matrix.T))
 
 # Simple multiplication
 
 print(matrix * matrix)
 print(matrix * 2)
+print(matrix + 10)
