992d1f5c47257b136b0751eb4247af75e232ba7a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-10-22 23:58:02 -0500

    float arrays

--- numpy/basics.py
+++ numpy/basics.py
@@ -13,8 +13,10 @@ matrix = np.array([
     [1, 1, 3, 5, 8, 13],
     [1, 1, 3, 5, 8, 13],
 ])
-
 print(matrix)
+
+f_array = np.array([1, 5, 6, 7, 7], float)
+print(f_array)
 
 # Transpose
 print(matrix.T)
@@ -27,3 +29,4 @@ print(matrix.dot(matrix.T))
 print(matrix * matrix)
 print(matrix * 2)
 print(matrix + 10)
+print(matrix - 10)
